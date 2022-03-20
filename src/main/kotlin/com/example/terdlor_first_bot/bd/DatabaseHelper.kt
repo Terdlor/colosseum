cd5fbbaa5dde36ec0c858098ad690d7248e556c3
Terdlor3 <kontiewskij@yandex.ru>
2022-03-21 00:20:53 +0300

    bd version 1

--- src/main/kotlin/com/example/terdlor_first_bot/bd/DatabaseHelper.kt
+++ src/main/kotlin/com/example/terdlor_first_bot/bd/DatabaseHelper.kt
@@ -9,6 +9,7 @@ import com.example.terdlor_first_bot.bd.dao.impl.DbInfoDaoImpl
 import com.example.terdlor_first_bot.bd.dao.impl.MessageDaoImpl
 import com.example.terdlor_first_bot.bd.dao.impl.UserDaoImpl
 import com.example.terdlor_first_bot.bd.model.*
+import com.j256.ormlite.dao.Dao
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource
 import com.j256.ormlite.support.ConnectionSource
 import com.j256.ormlite.table.TableUtils
@@ -18,7 +19,7 @@ import org.springframework.beans.factory.annotation.Value
 class DatabaseHelper private constructor(){
 
     companion object {
-        const val DATABASE_VERSION = 0
+        private const val DATABASE_VERSION = 1L
 
         @Value("\${spring.datasource.username}")
         private val username: String = "sa"
@@ -32,24 +33,22 @@ class DatabaseHelper private constructor(){
         var connectionSource : JdbcPooledConnectionSource? = null
 
         private fun instance() : ConnectionSource {
-            //TODO проверка на пригодность
             if (connectionSource == null ||  !connectionSource?.isOpen("")!!) {
-            //todo удаление из памяти старого конекта
                 connectionSource = JdbcPooledConnectionSource(url, username, password)
                 when(DbInfoDaoImpl(connectionSource).lastVersion()) {
-                           0L -> {
-                               TableUtils.createTableIfNotExists(connectionSource, DbInfo::class.java)
-                               TableUtils.createTableIfNotExists(connectionSource, User::class.java)
-                               TableUtils.createTableIfNotExists(connectionSource, Chat::class.java)
-                               TableUtils.createTableIfNotExists(connectionSource, Message::class.java)
-                               DbInfoDaoImpl(connectionSource).save(1)
-                           }
+                    0L -> {
+                        TableUtils.createTableIfNotExists(connectionSource, DbInfo::class.java)
+                        TableUtils.createTableIfNotExists(connectionSource, User::class.java)
+                        TableUtils.createTableIfNotExists(connectionSource, Chat::class.java)
+                        TableUtils.createTableIfNotExists(connectionSource, Message::class.java)
+                        DbInfoDaoImpl(connectionSource).save(DATABASE_VERSION)
+                        println("---обновление до $DATABASE_VERSION---")
+                    }
                     else -> {
                         println("---действий по изменению бд не требуется---")
                     }
                 }
             }
-            //TODO проверка на консистентность
             return connectionSource ?: JdbcPooledConnectionSource(url, username, password)
         }
 
@@ -67,10 +66,6 @@ class DatabaseHelper private constructor(){
 
         fun getMessageDao() : MessageDao {
             return MessageDaoImpl(instance())
-        }
-
-        fun getDbInfoDao() : DbInfoDao {
-            return DbInfoDaoImpl(instance())
         }
     }
 }
