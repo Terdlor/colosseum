d670dfc31b150bd88893e915fd827a9d2fa3fde0
Terdlor3 <kontiewskij@yandex.ru>
2022-12-25 02:40:43 +0300

    companion object DatabaseHelper

--- src/main/kotlin/com/example/terdlor_first_bot/bd/DatabaseHelper.kt
+++ src/main/kotlin/com/example/terdlor_first_bot/bd/DatabaseHelper.kt
@@ -32,108 +32,111 @@ import com.j256.ormlite.support.ConnectionSource
 import com.j256.ormlite.table.TableUtils
 import org.springframework.beans.factory.annotation.Value
 
-object DatabaseHelper {
+class DatabaseHelper private constructor() {
 
-    @Value("\${spring.datasource.username}")
-    private val username: String = "sa"
+    companion object {
 
-    @Value("\${spring.datasource.password}")
-    private val password: String = "111"
+        @Value("\${spring.datasource.username}")
+        private val username: String = "sa"
 
-    @Value("\${spring.datasource.url}")
-    private val url: String = "jdbc:h2:file:./data/testdb;AUTO_SERVER=TRUE"
+        @Value("\${spring.datasource.password}")
+        private val password: String = "111"
 
-    private var connectionSource: JdbcPooledConnectionSource? = null
+        @Value("\${spring.datasource.url}")
+        private val url: String = "jdbc:h2:file:./data/testdb"
 
-    private fun instance(): ConnectionSource {
-        if (connectionSource == null || !connectionSource?.isOpen("")!!) {
-            connectionSource = JdbcPooledConnectionSource(url, username, password)
-            //truncateAll()
-            when (DbInfoDaoImpl(connectionSource).lastVersion()) {
-                0L -> update1()
-                1L -> update2()
-                2L -> update3()
-                else -> println("---действий по изменению бд не требуется---")
+        private var connectionSource: JdbcPooledConnectionSource? = null
+
+        private fun instance(): ConnectionSource {
+            if (connectionSource == null || !connectionSource?.isOpen("")!!) {
+                connectionSource = JdbcPooledConnectionSource(url, username, password)
+                //truncateAll()
+                when (DbInfoDaoImpl(connectionSource).lastVersion()) {
+                    0L -> update1()
+                    1L -> update2()
+                    2L -> update3()
+                    else -> println("---действий по изменению бд не требуется---")
+                }
             }
+            return connectionSource!!
         }
-        return connectionSource!!
-    }
 
-    fun close() {
-        connectionSource?.close()
-    }
+        fun close() {
+            connectionSource?.close()
+        }
 
-    fun getUserDao() = UserDaoImpl(instance())
-    fun getChatDao() = ChatDaoImpl(instance())
-    fun getMessageDao() = MessageDaoImpl(instance())
+        fun getUserDao() = UserDaoImpl(instance())
+        fun getChatDao() = ChatDaoImpl(instance())
+        fun getMessageDao() = MessageDaoImpl(instance())
 
-    fun getHeroDao() = HeroDaoImpl(instance())
-    fun getPlayerDao() = PlayerDaoImpl(instance())
-    fun getBattleDao() = BattleDaoImpl(instance())
-    fun getBattleStepDao() = BattleStepDaoImpl(instance())
+        fun getHeroDao() = HeroDaoImpl(instance())
+        fun getPlayerDao() = PlayerDaoImpl(instance())
+        fun getBattleDao() = BattleDaoImpl(instance())
+        fun getBattleStepDao() = BattleStepDaoImpl(instance())
 
-    fun getBrandDao() = BrandDaoImpl(instance())
-    fun getLineDao() = LineDaoImpl(instance())
-    fun getTagDao() = TagDaoImpl(instance())
-    fun getTobaccoDao() = TobaccoDaoImpl(instance())
-    fun getTobaccoTagDao() = TobaccoTagDaoImpl(instance())
+        fun getBrandDao() = BrandDaoImpl(instance())
+        fun getLineDao() = LineDaoImpl(instance())
+        fun getTagDao() = TagDaoImpl(instance())
+        fun getTobaccoDao() = TobaccoDaoImpl(instance())
+        fun getTobaccoTagDao() = TobaccoTagDaoImpl(instance())
 
-    private fun update1() {
-        TableUtils.createTableIfNotExists(connectionSource, DbInfo::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, User::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, Chat::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, Message::class.java)
-        DbInfoDaoImpl(connectionSource).save(1)
-        println("---обновление бд до v1---")
-        update2()
-    }
+        private fun update1() {
+            TableUtils.createTableIfNotExists(connectionSource, DbInfo::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, User::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Chat::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Message::class.java)
+            DbInfoDaoImpl(connectionSource).save(1)
+            println("---обновление бд до v1---")
+            update2()
+        }
 
-    private fun update2() {
-        val dao = DbInfoDaoImpl(connectionSource)
-        dao.executeRaw("ALTER TABLE `USERS` ADD COLUMN IF NOT EXISTS insert_date TIMESTAMP;")
-        dao.executeRaw("ALTER TABLE `CHATS` ADD COLUMN IF NOT EXISTS insert_date TIMESTAMP;")
-        dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS insert_date TIMESTAMP;")
-        dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS rq VARCHAR2(4096);")
-        dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS rs VARCHAR2(4096);")
-        dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS rs_chat_id VARCHAR(255);")
-        dao.save(2)
-        println("---обновление бд до v2---")
-        update3()
-    }
+        private fun update2() {
+            val dao = DbInfoDaoImpl(connectionSource)
+            dao.executeRaw("ALTER TABLE `USERS` ADD COLUMN IF NOT EXISTS insert_date TIMESTAMP;")
+            dao.executeRaw("ALTER TABLE `CHATS` ADD COLUMN IF NOT EXISTS insert_date TIMESTAMP;")
+            dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS insert_date TIMESTAMP;")
+            dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS rq VARCHAR2(4096);")
+            dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS rs VARCHAR2(4096);")
+            dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS rs_chat_id VARCHAR(255);")
+            dao.save(2)
+            println("---обновление бд до v2---")
+            update3()
+        }
 
-    private fun update3() {
-        TableUtils.createTableIfNotExists(connectionSource, Player::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, Hero::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, Battle::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, BattleStep::class.java)
+        private fun update3() {
+            TableUtils.createTableIfNotExists(connectionSource, Player::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Hero::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Battle::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, BattleStep::class.java)
 
-        TableUtils.createTableIfNotExists(connectionSource, Brand::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, Line::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, Tag::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, Tobacco::class.java)
-        TableUtils.createTableIfNotExists(connectionSource, TobaccoTag::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Brand::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Line::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Tag::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, Tobacco::class.java)
+            TableUtils.createTableIfNotExists(connectionSource, TobaccoTag::class.java)
 
-        val dao = DbInfoDaoImpl(connectionSource)
-        dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS caption VARCHAR(255);")
+            val dao = DbInfoDaoImpl(connectionSource)
+            dao.executeRaw("ALTER TABLE `MESSAGE` ADD COLUMN IF NOT EXISTS caption VARCHAR(255);")
 
-        DbInfoDaoImpl(connectionSource).save(3)
-        println("---обновление бд до v3---")
-    }
+            DbInfoDaoImpl(connectionSource).save(3)
+            println("---обновление бд до v3---")
+        }
 
-    private fun truncateAll() {
-        TableUtils.dropTable(DbInfoDaoImpl(connectionSource), true)
-        TableUtils.dropTable(getUserDao(), true)
-        TableUtils.dropTable(getChatDao(), true)
-        TableUtils.dropTable(getMessageDao(), true)
-        TableUtils.dropTable(getHeroDao(), true)
-        TableUtils.dropTable(getPlayerDao(), true)
-        TableUtils.dropTable(getBattleDao(), true)
-        TableUtils.dropTable(getBattleStepDao(), true)
-        TableUtils.dropTable(getBrandDao(), true)
-        TableUtils.dropTable(getLineDao(), true)
-        TableUtils.dropTable(getTagDao(), true)
-        TableUtils.dropTable(getTobaccoDao(), true)
-        TableUtils.dropTable(getTobaccoTagDao(), true)
-        println("---бд очищена---")
+        private fun truncateAll() {
+            TableUtils.dropTable(DbInfoDaoImpl(connectionSource), true)
+            TableUtils.dropTable(getUserDao(), true)
+            TableUtils.dropTable(getChatDao(), true)
+            TableUtils.dropTable(getMessageDao(), true)
+            TableUtils.dropTable(getHeroDao(), true)
+            TableUtils.dropTable(getPlayerDao(), true)
+            TableUtils.dropTable(getBattleDao(), true)
+            TableUtils.dropTable(getBattleStepDao(), true)
+            TableUtils.dropTable(getBrandDao(), true)
+            TableUtils.dropTable(getLineDao(), true)
+            TableUtils.dropTable(getTagDao(), true)
+            TableUtils.dropTable(getTobaccoDao(), true)
+            TableUtils.dropTable(getTobaccoTagDao(), true)
+            println("---бд очищена---")
+        }
     }
 }
